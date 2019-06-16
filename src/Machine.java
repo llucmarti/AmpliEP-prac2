b5747fa5bda4187311926fd041c9cb7734723e32
llucmarti <47560772+llucmarti@users.noreply.github.com>
2019-06-16 17:20:26 +0200

    apartat a

--- src/Machine.java
+++ src/Machine.java
@@ -1,5 +1,25 @@
 public class Machine extends MachineComponent {
 
+    private boolean wasBroken;
+
+    public void setBroken(){
+        wasBroken = broken;
+        broken = true;
+        if(!wasBroken){
+            setChanged();
+            notifyObservers();
+        }
+    }
+
+    public void repair() {
+        wasBroken = broken;
+        broken = false;
+        if(wasBroken){
+            setChanged();
+            notifyObservers();
+        }
+    }
+
     public boolean isBroken() {
         return broken;
     }
